c01eca2c76b77938eb892f742eab7174fc988fd3
Yago Moreira <yagomoreira@gmail.com>
2024-08-04 16:09:46 -0300

    refactor: não renderiza orders que não possuem os atributos necessarios

--- frontend/components/Table.vue
+++ frontend/components/Table.vue
@@ -28,6 +28,7 @@ export default defineComponent({
     const ordersData = ref<OrderNf[]>([]);
     const isOpen = ref(false);
     const cedenteData = ref<Cedente>(initialCedente);
+    const filteredData = ref([]);
     const statusMapper = [
       "Pendente de confirmação",
       "Pedido confirmado",
@@ -63,8 +64,9 @@ export default defineComponent({
     onMounted(async () => {
       await fetchData();
       if (data.value) {
-        ordersData.value = data.value.map((item) => {
-          return {
+        filteredData.value = data.value.filter((item) => item.nNf && item.value && item.buyer && item.provider && item.emissionDate && item.orderStatusBuyer)
+        ordersData.value = filteredData.value.map((item) => {
+            return {
             nNf: item.nNf,
             value: item.value,
             sacado: item.buyer,
@@ -72,7 +74,7 @@ export default defineComponent({
             emissionDate: item.emissionDate,
             status: item.orderStatusBuyer,
           };
-        });
+        })
       }
     });
 
@@ -92,72 +94,72 @@ export default defineComponent({
 });
 </script>
 <template>
+  <div class="flex flex-col gap-4">
+    <div class="flex px-4 gap-16 items-center mt-5">
+      <span class="font-dmsans text-xs uppercase text-zinc-400 w-20"
+        >Nota fiscal</span
+      >
+      <span class="font-dmsans text-xs uppercase text-zinc-400 w-24"
+        >Sacado</span
+      >
+      <span class="font-dmsans text-xs uppercase text-zinc-400 w-24"
+        >Cedente</span
+      >
+      <span class="font-dmsans text-xs uppercase text-zinc-400 w-20"
+        >Emissão</span
+      >
+      <span class="font-dmsans text-xs uppercase text-zinc-400 w-32"
+        >Valor</span
+      >
+      <span class="font-dmsans text-xs uppercase text-zinc-400 w-56"
+        >Status</span
+      >
+    </div>
     <div class="flex flex-col gap-4">
-      <div class="flex px-4 gap-16 items-center mt-5">
-        <span class="font-dmsans text-xs uppercase text-zinc-400 w-20"
-          >Nota fiscal</span
-        >
-        <span class="font-dmsans text-xs uppercase text-zinc-400 w-24"
-          >Sacado</span
-        >
-        <span class="font-dmsans text-xs uppercase text-zinc-400 w-24"
-          >Cedente</span
-        >
-        <span class="font-dmsans text-xs uppercase text-zinc-400 w-20"
-          >Emissão</span
-        >
-        <span class="font-dmsans text-xs uppercase text-zinc-400 w-32"
-          >Valor</span
-        >
-        <span class="font-dmsans text-xs uppercase text-zinc-400 w-56"
-          >Status</span
-        >
-      </div>
-      <div class="flex flex-col gap-4">
+      <div
+        v-for="item in ordersData"
+        :key="item.nNf"
+        class="w-full flex items-center gap-16 px-4 py-2 h-12 border-[1px] border-[#DFE2EB] rounded-lg"
+      >
         <div
-          v-for="item in ordersData"
-          :key="item.nNf"
-          class="w-full flex items-center gap-16 px-4 py-2 h-12 border-[1px] border-[#DFE2EB] rounded-lg"
+          class="text-[#4D5566] font-dmsans text-sm font-medium text-left uppercase w-20"
         >
-          <div
-            class="text-[#4D5566] font-dmsans text-sm font-medium text-left uppercase w-20"
-          >
-            {{ item.nNf }}
-          </div>
-          <div
-            class="text-[#4D5566] font-dmsans text-sm font-medium text-left uppercase w-24 truncate"
-          >
-            {{ item.sacado.name }}
-          </div>
-          <div
-            class="text-[#4D5566] font-dmsans text-sm font-medium text-left uppercase w-24 truncate"
-          >
-            {{ item.cedente.name }}
-          </div>
-          <div
-            class="text-[#4D5566] font-dmsans text-sm font-medium text-left uppercase w-20"
-          >
-            {{ formatDate(item.emissionDate) }}
-          </div>
-          <div
-            class="text-[#00AD8C] font-dmsans text-sm font-medium text-left uppercase w-32"
-          >
-            {{ currency.format(Number(item.value)) }}
-          </div>
-          <div
-            class="text-[#00AD8C] font-dmsans text-xs font-bold text-left uppercase w-56 truncate"
-          >
-            {{ statusMapper[Number(item.status)] }}
-          </div>
-          <UButton
-            class="bg-transparent text-[#727D94] border-[#CAD3FF] border-[1px] rounded-2xl font-dmsans text-xs hover:bg-zinc-200 mr-3"
-            @click="openModal(item.cedente)"
-          >
-            Dados do cedente
-          </UButton>
+          {{ item.nNf }}
         </div>
+        <div
+          class="text-[#4D5566] font-dmsans text-sm font-medium text-left uppercase w-24 truncate"
+        >
+          {{ item.sacado.name }}
+        </div>
+        <div
+          class="text-[#4D5566] font-dmsans text-sm font-medium text-left uppercase w-24 truncate"
+        >
+          {{ item.cedente.name }}
+        </div>
+        <div
+          class="text-[#4D5566] font-dmsans text-sm font-medium text-left uppercase w-20"
+        >
+          {{ formatDate(item.emissionDate) }}
+        </div>
+        <div
+          class="text-[#00AD8C] font-dmsans text-sm font-medium text-left uppercase w-32"
+        >
+          {{ currency.format(Number(item.value)) }}
+        </div>
+        <div
+          class="text-[#00AD8C] font-dmsans text-xs font-bold text-left uppercase w-56 truncate"
+        >
+          {{ statusMapper[Number(item.status)] }}
+        </div>
+        <UButton
+          class="bg-transparent text-[#727D94] border-[#CAD3FF] border-[1px] rounded-2xl font-dmsans text-xs hover:bg-zinc-200 mr-3"
+          @click="openModal(item.cedente)"
+        >
+          Dados do cedente
+        </UButton>
       </div>
     </div>
+  </div>
 
   <CedenteModal
     :isOpen="isOpen"
